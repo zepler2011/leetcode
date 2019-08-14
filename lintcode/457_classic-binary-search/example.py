d19c36f4d68866544b13f5947e49e495e4ee2275
Zach Shen <zshen@vmware.com>
2019-08-14 16:23:57 -0700

    Lintcode: add 457: binary search

---
+++ lintcode/457_classic-binary-search/example.py
@@ -0,0 +1,26 @@
+class Solution:
+    """
+    @param nums: An integer array sorted in ascending order
+    @param target: An integer
+    @return: An integer
+    """
+    def findPosition(self, nums, target):
+        # write your code here
+
+        if len(nums) == 0:
+            return -1
+
+        start, end = 0, len(nums) - 1
+        while start + 1 < end:
+            mid = (start + end) //2
+            if nums[mid] < target:
+                start = mid
+            elif nums[mid] >= target:
+                end = mid
+
+        if nums[start] == target:
+            return start
+        if nums[end] == target:
+            return end
+
+        return -1
